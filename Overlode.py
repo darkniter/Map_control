a6a342300ee5c72251321bd15c4e6442f7341882
darkniter <Poranot@yandex.ru>
2019-10-14 11:56:28 +0300

    First commit

---
+++ Overlode.py
@@ -0,0 +1,18 @@
+from lxml import etree
+import json
+import os
+import config
+
+def main():
+    old_map = load_old_map()
+
+    return old_map
+
+
+def load_old_map(old_map_file = config.OLDMAPFILE):
+    old_map = None
+    return old_map
+
+
+if __name__ == "__main__":
+    main()
